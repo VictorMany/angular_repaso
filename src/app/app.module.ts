479ac7eefe8b87729cfd3c934a0b080b63887858
VictorMany <victor.velazfuentez@gmail.com>
2021-03-03 00:33:30 -0600

    Estructura básica

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DishesListComponent } from './components/dishes-list/dishes-list.component';
+///////////////////////////
+// Módulo que nos permite hacer peticiones http
+import { HttpClientModule } from '@angular/common/http';
+
+// Ennvironment
+import { environment } from '../environments/environment';
+
+// Modulos de firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DishesListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // Conexión con un proyecto de firebase
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    // Modulo de autenticación con firebase
+    AngularFireAuthModule,
+    // Módulo para trabajar con la base de datos
+    AngularFirestoreModule,
+    // Módulo para alamcenar archivos en firebase
+    AngularFireStorageModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC8qLqv0XZmwhM8mO66op8APz7FrqsN-Hc",
+    authDomain: "prueba-a4c30.firebaseapp.com",
+    databaseURL: "https://prueba-a4c30-default-rtdb.firebaseio.com",
+    projectId: "prueba-a4c30",
+    storageBucket: "prueba-a4c30.appspot.com",
+    messagingSenderId: "218602599538",
+    appId: "1:218602599538:web:c08132ca2632681e497eeb"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
